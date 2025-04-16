b4a27845a0f0ca2033043e070ecc4de679a28341
Teddyburgonde <bandama.teddy@gmail.com>
2025-04-16 20:10:40 +0200

    to do makeWithdrawal function

--- cpp00bonus/Account.cpp
+++ cpp00bonus/Account.cpp
@@ -1,6 +1,4 @@
 #include "Account.hpp"
-#include <iostream>
-#include <ctime>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
@@ -11,14 +9,57 @@ int Account::_totalNbWithdrawals = 0;
 Account::Account(int initial_deposit): _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
 {
 	_totalAmount += initial_deposit;
+	_nbAccounts++;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+          << ";amount:" << _amount
+          << ";created" << std::endl;
 }
 Account::~Account(void)
 {
-
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+          << ";amount:" << _amount
+          << ";closed" << std::endl;
 }
 
+void	Account::_displayTimestamp(void)
+{
+	std::time_t now = std::time(0);
+	std::tm *ltm = std::localtime(&now);
+	int year = 1900 + ltm->tm_year;
+	int month = 1 + ltm->tm_mon;
+	int day = ltm->tm_mday;
+	int hour = ltm->tm_hour;
+	int min = ltm->tm_min;
+	int sec = ltm->tm_sec;
 
+	std::cout << "[" << year;
 
+	if (month < 10)
+		std::cout << "0";
+	std::cout << month;
+	
+	if (day < 10)
+		std::cout << "0";
+	std::cout << day;
+	
+	std::cout << "_";
+	
+	if (hour < 10)
+		std::cout << "0";
+	std::cout << hour;
+	
+	if (min < 10)
+		std::cout << "0";
+	std::cout << min;
+	
+	if (sec < 10)
+		std::cout << "0";
+	std::cout << sec;
+	
+	std::cout << "] ";
+}
 
 // Functions
 
@@ -29,14 +70,14 @@ void Account::makeDeposit(int deposit)
 	_totalNbDeposits++;
 	_totalAmount+= deposit;
 }
-bool	Account::makeWithdrawal(int withdrawal)
-{
+// bool	Account::makeWithdrawal(int withdrawal)
+// {
 
-}
-int		Account::checkAmount(void) const
-{
+// }
+// int		Account::checkAmount(void) const
+// {
 
-}
+// }
 
 void	Account::displayStatus( void ) const
 {
